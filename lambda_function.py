6b03c18e79904ccb1be6587d67387656f57ed29b
Nikhil Wagh <nikhilwagh.work@gmail.com>
2018-04-07 09:25:00 +0530

    GetTopCharts working fine for now

--- lambda_function.py
+++ lambda_function.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import decimal
 from numpy import random
+import datetime
 
 # python-lambda-local -f lambda_handler -t 10 lambda_function.py ./events/event.json
 
@@ -64,7 +65,7 @@ def GetTrendingTopics(request, attributes):
 		title, articleTitle, article = getCurrentTopic(trending_searches_df, attributes)
 		if articleTitle == "EMPTY":
 			return response_plain_text("Woooow, you have gone through all of our trending topics. Would you like to look for them again? Or you can always come later.",
-			 False, attributes, "I'm out of topics", "I have this problem, I'm very sorry.",
+			 True, attributes, "I'm out of topics", "I have this problem, I'm very sorry.",
 			 "I can tell you again, if you want."
 			)
 		outputSpeech += (articleTitle + " ")
@@ -105,9 +106,115 @@ def setIndex(index, attributes):
 	return attributes
 
 
-def GetTopCharts(request):
+def GetTopCharts(request, i = 0):
 	pytrend = TrendReq()
-	top_charts_df = pytrend.top_charts()
+	keyword = getSlotValue(request['intent'], 'WORD')
+	cid, keyword = getCid(request['intent'], 'WORD')
+
+	# Validations
+	if cid == -1:
+		return getKeywordError()
+	date = getDate(request['intent'], 'DATE')
+	if date == -1:
+		date = int(datetime.datetime.now().strftime ("%Y"))
+	if cid == -1:
+		return response_plain_text("Please provide proper top charts topic.",
+		False,
+		"Incorrect slot value",
+		"Sorry, I can't understand what you were looking for.",
+		"Would you like to hear about " + getRandomKeyword())
+
+	try:
+		date = date - i
+		print("\ncid ::", cid, "\nDate ::", date)
+		top_charts_df = pytrend.top_charts(cid = cid, date=date)
+	except Exception as e:
+		if i == 0:
+			return GetTopCharts(request, i + 1)
+		else:
+		    print(type(e))
+		    print(e.args)
+		    return response_plain_text("Sorry, I think I'm not feeling well today. I can't answer that", True, {}, "Our apologies", "Sorry, for the inconvenience", "You can try again if you want.")
+	else:
+		outputSpeech = "Here are some top " + keyword + " ... "
+		cardContent = ""
+		i = 1
+		for title in top_charts_df['title']:
+			if i >= 10:
+				break;
+			outputSpeech += title + ", "
+			cardContent += title + "\n"
+			i += 1
+		return response_plain_text(outputSpeech, False, {}, "Top charts of " + keyword, cardContent, "Would you like to hear about " + getRandomKeyword())
+   		
+
+def getKeywordError():
+	Messages = ["Sorry, the word you asked for is not available."]
+	return response_plain_text(getRandomKeyword(Messages), False, {}, "Keyword error", "Cannot find the keyword to search for.", "Would you like to hear the available topics?")
+
+
+def getDate(intent, slot):
+	slot = getSlotValue(intent, 'DATE')
+	if type(slot) == str:
+		slot = slot.split(' ')
+		slot = ''.join(slot)
+	return int(slot)
+
+
+def getCid(intent, keyword):
+	slot = getSlotValue(intent, keyword)
+	if slot != -1:
+		slot = slot.lower()
+		slot = slot.split(' ')
+		slot = ''.join(slot)
+		if slot == 'games' or slot == 'game':
+			return 'games', slot
+		elif slot == 'actor' or slot == 'actors':
+			return 'actors', slot
+		elif slot == 'politicians' or slot == 'leaders':
+			return 'politicians', slot
+		elif slot == 'scientists':
+			return 'scientists', slot
+		elif slot == 'cars' or slot == 'sportscars':
+			return 'sports_cars', slot
+		elif slot == 'serials' or slot == 'seasons' or slot == 'realityshows':
+			return 'reality_shows', slot
+		elif slot == 'food' or slot == 'foods' :
+			return 'foods', slot
+		elif slot == 'books' or slot == 'novels' or slot == 'comics':
+			return 'books', slot
+		elif slot == 'authors' or slot == 'writers':
+			return 'authors', slot
+		elif slot == 'songs' or slot == 'pop':
+			return 'songs', slot
+		elif slot == 'dogs' or slot == 'dogbreeds':
+			return 'dog_breeds', slot
+		elif slot == 'athletes':
+			return 'athletes', slot
+		elif slot == 'people' :
+			return 'people', slot
+		else :
+			return -1, -1
+
+
+def getRandomKeyword():
+	Keywords = [
+		'games',
+		'actors',
+		'politicians',
+		'scientists',
+		'sports cars',
+		'reality shows',
+		'foods',
+		'books',
+		'authors',
+		'songs',
+		'dog breeds',
+		'athletes',
+		'people'
+	]
+	return getRandom(Keywords)
+
 
 def GetRelatedTopics(request):
 	pytrend = TrendReq()
--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
-zip  -r my_lambda.zip . --exclude events/ --exclude  upload.sh
+zip  -r my_lambda.zip . --exclude events/ --exclude  upload.sh --exclude *.git*
 aws s3 cp my_lambda.zip s3://trendingbucket/
 aws lambda update-function-code --function-name "trending-function" --s3-bucket "trendingbucket" --s3-key "my_lambda.zip"
 rm my_lambda.zip
